aac85696a760badc58d61b2a6e4743bbc71e7fc6
Andrew Dawson <dawso.andrew@gmail.com>
2022-11-04 14:51:47 -0400

    Adds a mock to the weather alerts test.

--- create-react-app/src/Forecast/WeatherAlerts.test.tsx
+++ create-react-app/src/Forecast/WeatherAlerts.test.tsx
@@ -1,5 +1,17 @@
 import { render, screen } from "../Common/TestUtilities";
 import { WeatherAlerts } from "./WeatherAlerts";
+import * as nwsClient from "@vavassor/nws-client";
+
+jest.mock("@vavassor/nws-client", () => {
+  const { jest } = require("@jest/globals");
+  return {
+    __esModule: true,
+    ...jest.requireActual("@vavassor/nws-client"),
+    getActiveAlerts: jest.fn(),
+  };
+});
+
+export const mockedNwsClient = nwsClient as jest.Mocked<typeof nwsClient>;
 
 describe("WeatherAlerts", () => {
   test("shows the heading", () => {
