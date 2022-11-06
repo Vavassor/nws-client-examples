64774ef1876cb15b8e466bff80a6eb773e7d4bae
Andrew Dawson <dawso.andrew@gmail.com>
2022-11-06 01:44:13 -0500

    Adds react router to the CRA example.

--- create-react-app/src/App.tsx
+++ create-react-app/src/App.tsx
@@ -1,56 +1,22 @@
-import React from "react";
-import { CurrentConditions } from "./Forecast/CurrentConditions";
-import { HourlyWeather } from "./Forecast/HourlyWeather";
-import { WeatherAlerts } from "./Forecast/WeatherAlerts";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {
-  Box,
-  ChakraProvider,
-  Container,
-  Grid,
-  GridItem,
-  Heading,
-} from "@chakra-ui/react";
-import { SkipNavLink, SkipNavContent } from "@chakra-ui/skip-nav";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./Root";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
-        <SkipNavLink>Skip to content</SkipNavLink>
-        <Box
-          as="header"
-          backgroundColor="white"
-          borderBottomWidth={1}
-          paddingX={6}
-          paddingY={3}
-        >
-          <Heading as="h1" size="xl">
-            Weather
-          </Heading>
-        </Box>
-        <Container as="main" maxW="container.sm" pt={4}>
-          <SkipNavContent />
-          <Grid
-            gap={4}
-            maxWidth="100%"
-            templateAreas={`"conditions"
-            "alerts"
-            "hourly"`}
-          >
-            <GridItem area="conditions">
-              <CurrentConditions />
-            </GridItem>
-            <GridItem area="alerts">
-              <WeatherAlerts />
-            </GridItem>
-            <GridItem area="hourly">
-              <HourlyWeather />
-            </GridItem>
-          </Grid>
-        </Container>
+        <RouterProvider router={router} />
       </ChakraProvider>
     </QueryClientProvider>
   );
