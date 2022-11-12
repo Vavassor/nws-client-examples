ec312f71b55f47860ba3efc409832e53f5e0940b
Andrew Dawson <dawso.andrew@gmail.com>
2022-11-12 05:25:38 -0500

    Adds links to glossary terms.

--- create-react-app/src/Glossary/GlossarySection.tsx
+++ create-react-app/src/Glossary/GlossarySection.tsx
@@ -6,21 +6,31 @@ import {
   FormLabel,
   Heading,
   Input,
+  Link,
+  Skeleton,
+  SkeletonText,
   Text,
 } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { getGlossary } from "@vavassor/nws-client";
-import { FC, Fragment, useMemo } from "react";
+import { FC, Fragment, useEffect, useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { useSearchParams } from "react-router-dom";
+import {
+  Link as RouterLink,
+  useLocation,
+  useSearchParams,
+} from "react-router-dom";
+import { sanitizeId } from "../Common/ElementId";
 
 interface FormValues {
   search: string;
 }
 
 export const GlossarySection: FC = () => {
-  const { data: glossary } = useQuery(["glossary"], () => getGlossary({}));
+  const { data: glossary, isLoading } = useQuery(["glossary"], () =>
+    getGlossary({})
+  );
   const { t } = useTranslation("glossary");
   const [urlSearchParams, setUrlSearchParams] = useSearchParams();
   const {
@@ -28,6 +38,7 @@ export const GlossarySection: FC = () => {
     register,
     formState: { errors, isSubmitting },
   } = useForm<FormValues>();
+  const { hash } = useLocation();
 
   const onSubmit = handleSubmit((values) => {
     setUrlSearchParams(values.search ? { query: values.search } : undefined);
@@ -46,6 +57,13 @@ export const GlossarySection: FC = () => {
     [glossary, urlSearchParams]
   );
 
+  useEffect(() => {
+    if (glossary) {
+      const element = document.getElementById(hash.slice(1));
+      element?.scrollIntoView(true);
+    }
+  }, [glossary, hash]);
+
   return (
     <Box as="section" borderRadius="lg" borderWidth="1px" px={8} py={4}>
       <Heading as="h1" size="lg">
@@ -63,19 +81,43 @@ export const GlossarySection: FC = () => {
           {t("glossarySection.submitButtonLabel")}
         </Button>
       </form>
-      <dl>
-        {filteredDefinitions &&
-          filteredDefinitions.map((definition) => (
-            <Fragment
-              key={`${definition.term}-${definition.definition.length}`}
-            >
-              <Text as="dt" fontWeight="semibold" mt={4}>
-                {definition.term}
-              </Text>
-              <dd dangerouslySetInnerHTML={{ __html: definition.definition }} />
-            </Fragment>
-          ))}
-      </dl>
+      {filteredDefinitions && filteredDefinitions.length > 0 ? (
+        <dl>
+          {filteredDefinitions.map((definition) => {
+            const id = `${sanitizeId(definition.term)}-${
+              definition.definition.length
+            }`;
+            return (
+              <Fragment key={id}>
+                <Text as="dt" data-group fontWeight="semibold" id={id} mt={4}>
+                  {definition.term}
+                  <Link
+                    aria-label={t("glossarySection.termLink")}
+                    as={RouterLink}
+                    _groupHover={{ opacity: 1 }}
+                    opacity={0}
+                    to={`#${id}`}
+                  >
+                    #
+                  </Link>
+                </Text>
+                <dd
+                  dangerouslySetInnerHTML={{ __html: definition.definition }}
+                />
+              </Fragment>
+            );
+          })}
+        </dl>
+      ) : isLoading ? (
+        <>
+          <Skeleton height="20px" mt={4} />
+          <SkeletonText noOfLines={4} spacing="4" />
+        </>
+      ) : (
+        <Text mb={6} mt={8}>
+          {t("glossarySection.noResultsMessage")}
+        </Text>
+      )}
     </Box>
   );
 };
